6f68a06417cdf1f6ffe5f6fe6918fce7b423ae56
MarcM987 <marcmartinez987@gmail.com>
2020-05-23 10:32:39 -0700

    firebase added

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAmPHWsYDuAeFKVaMnAlIHO96CVG0kXRZs",
+    authDomain: "groupproject1-73c74.firebaseapp.com",
+    databaseURL: "https://groupproject1-73c74.firebaseio.com",
+    projectId: "groupproject1-73c74",
+    storageBucket: "groupproject1-73c74.appspot.com",
+    messagingSenderId: "398999884281",
+    appId: "1:398999884281:web:7eac86826ce3abc07fad28",
+    measurementId: "G-VQXFZW71R4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
